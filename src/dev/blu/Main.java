a8bd744fecce20f5a8edbc6316a9a770e0992dc3
Blu Borghi <blu.borghi@gmail.com>
2019-11-21 18:58:30 +0100

    Command line file splitter

--- src/dev/blu/Main.java
+++ src/dev/blu/Main.java
@@ -1,9 +1,38 @@
 package dev.blu;
 
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        String fileDir = args[0];
+        int parts = Integer.parseInt(args[1]);
 
-        
+        File inputFile = new File(fileDir);
+        int part_length = (int) (inputFile.length()/parts)+1;
+
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(inputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int i = 0;
+        byte[] buffer = new byte[part_length];
+        try {
+            while (fis.read(buffer)!=-1){
+                File outputFile = new File(inputFile.getAbsolutePath()+inputFile.getName()+"_"+ i +".part");
+                i++;
+                outputFile.createNewFile();
+                FileOutputStream fos = new FileOutputStream(outputFile);
+                fos.write(buffer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
