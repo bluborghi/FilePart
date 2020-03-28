8519a8ff51b9bbf319994e296a4d51764c899f7f
Blu Borghi <blu.borghi@gmail.com>
2020-03-28 12:15:28 +0100

    implement AppModel and FileConfiguration, fix FileTableModel

--- src/dev/blu/model/AppModel.java
+++ src/dev/blu/model/AppModel.java
@@ -1,5 +1,33 @@
 package dev.blu.model;
 
-public class AppModel {
+import java.io.File;
+import java.util.UUID;
 
+import dev.blu.model.output.FileTableModel;
+
+public class AppModel {
+	FileTableModel tableModel;
+	
+	public AppModel() {
+		tableModel = new FileTableModel();
+	}
+	
+	public UUID addFile(File file) {
+		UUID id = UUID.randomUUID();
+		tableModel.addFile(file, id);
+		return id;
+	}
+	
+	public File removeFileAt(int index) {
+		File f = tableModel.getConfig(index).getFile();
+		if (tableModel.removeFileAt(index) == 0) {
+			return f; //return removed file
+		}
+		return null; //if no file was found
+	}
+
+	public File removeFile(UUID id) {
+		return removeFileAt(tableModel.getIndex(id));
+	}
+	
 }
